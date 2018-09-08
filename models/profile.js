2efa2195129f375703990eb1490c9aa01e6ffb27
victoriaashling <victoria.ashling@gmail.com>
2018-09-07 23:43:07 -0400

    i think this will make it compatible with heroku again

--- models/profile.js
+++ models/profile.js
@@ -14,7 +14,7 @@ module.exports = function(sequelize, DataTypes) {
         namespace: {
             type: DataTypes.STRING
         },
-        UserId: {
+        userId: {
             type: DataTypes.INTEGER,
             allowNull: false
         }
